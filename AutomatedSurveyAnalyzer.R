08813bb576783cfa2cdb7f620971d059ee5e03ad
Martin Hayford <martinhayford7@gmail.com>
2023-08-11 16:57:48 +0300

    comments and a few more updates

--- AutomatedSurveyAnalyzer.R
+++ AutomatedSurveyAnalyzer.R
@@ -52,63 +52,63 @@ for (package in packages)
   }
 }
 
+#source the functions for this script from the dedicated function script
 source("ASAfunctions.R")
 
-#main df
+#Import and clean DataFrames
 df <- read.csv("SchoolLunchSurvey.csv")
-
 clean_df <- clean_df(df)
 
 scales <- as.list(read.csv("SchoolLunchScales.csv"))
-scales <- lapply(scales, function(z){ z[!is.na(z) & z != ""]})
+scales <- lapply(scales, function(z){ z[!is.na(z) & z != ""]}) #remove NA elements from the end of lists (artifact from importing procedure, all are imported as the same length)
 
 # Manual Inputs ----
 #column to start and end
-column_range_start <- 19
-column_range_end <- 25
+column_range_start <- 19 #first question
+column_range_end <- 25 #last question
+
+demoList <- c(3,2) #each column number that will be used for demographic analysis (in the desired order of appearance in the frequency table)
+
+
 column_range <- seq(from = column_range_start, to = column_range_end)
-
-demoList <- list(3,2)
-
-
-
-
 # analysis ----
 for (question in column_range)
 {
   #use cleaned_df to create frequency tables
   #Frequency function
   freq <- frequencies(df[question])
+
+  #add demographics
+  for (demo in demoList)
+  {
+    optionList <- unique(clean_df[[demo]])#enumerate all the options in the demographic (optionList)
+    
+    for (option in optionList)
+    {
+      #create a subset of the data based on the [option]
+      subset <- filter(clean_df, !!as.symbol(names(clean_df[demo])) == option) #ugly filter requiring changing datatype and syntactic sugar indicating that the internal functions should run before, not parallel to the external function
+      #do a frequency of that subset
+      freqD <- frequencies(subset[question])
+      #append that frequency row to the above frequency table
+      freq <- bind_rows(freq,freqD)
+    }
+  }
+  
   #sort columns, record the color palette
-  j=0
+  j=0 #reset counter variable (this allows us to refer to the column after the correctly matched scale by its index)
   for(scale in scales)
   {
     j <- j+1
     
-    if(identical(sort(scale),sort(colnames(freq))))
+    if(identical(sort(scale),sort(colnames(freq)))) #proceed if the scale of the frequency table matches the scale we are checkign against in the scales csv file
     {
-      freq <- freq[scale]
-      factors <- scale
-      customcolors <- unlist(scales[j+1])
-      names(customcolors) = scale
+      freq <- freq[scale] #this line orders the columns based on the order of the scale csv file
+      factors <- scale # this preserves the correct order for later when we must set the factor levels
+      customcolors <- unlist(scales[j+1]) #this creates an array of colors in the same order of the scale from the csv
+      names(customcolors) = scale # this names the elements of the array based on that scale
     }
   }
-  #add demographics
-  for (demo in demoList)
-  {
-    #enumerate all the options in the demographic (optionList)
-    optionList<- unique(clean_df[[demo]])
-    for (option in optionList)
-    {
-      #create a subset of the data based on the [option]
-      subset <- filter(clean_df, !!as.symbol(names(clean_df[demo])) == option)
-      #do a frequency of that subset
-      freqD <- frequencies(subset[question])
-      #sort columns
-      #append that frequency row to the above frequency table
-    }
-  }
-  
+  print(freq)
   #convert to percentage
   #hypothesis test
   #charts
